976af4370b33d463369204e1765f9110f2b7f54e
EnriqueSolarte <enrique.solarte.pardo@gmail.com>
2025-01-06 17:26:09 +0800

    Add example of how to project from depth and rgb into 3D

--- example/project_scene_in_3d.py
+++ example/project_scene_in_3d.py
@@ -1,5 +1,5 @@
 import hydra
-from geometry_perception_utils.io_utils import get_abs_path
+from geometry_perception_utils.io_utils import get_abs_path, load_module
 from geometry_perception_utils.pinhole_utils import project_pp_depth_from_hfov
 from geometry_perception_utils.image_utils import get_color_array
 from geometry_perception_utils.vispy_utils import plot_color_plc
@@ -19,16 +19,19 @@ def main(cfg):
     list_frames = [Path(f).stem for f in os.listdir(cfg.open_eqa.rgb_dir)]
     _xyz_rgb_wc = []
 
+    cam_projection = load_module(cfg.open_eqa.dataset.cam_projection_module)
+
     for fr in tqdm(list_frames[0:-1:5]):
         rgb = imread(f"{cfg.open_eqa.rgb_dir}/{fr}.jpg")
         depth = np.load(f"{cfg.open_eqa.depth_dir}/{fr}.npy")
         cam_pose = np.load(f"{cfg.open_eqa.poses_dir}/{fr}.npy")
 
-        xyz_cc, m = project_pp_depth_from_hfov(depth, cfg.open_eqa.hfov)
+        xyz_cc, m = cam_projection(depth, cfg.open_eqa)
         xyz_wc = cam_pose[:3, :] @ extend_array_to_homogeneous(xyz_cc)
         xyz_color = get_color_array(rgb)[:, m]/255
 
         _xyz_rgb_wc.append(np.vstack((xyz_wc, xyz_color)))
+        plot_color_plc(xyz_wc[:3, :].T, xyz_color.T)
 
     xyz_rgb_wc = np.hstack(_xyz_rgb_wc)
     plot_color_plc(xyz_rgb_wc[:3, :].T, xyz_rgb_wc[3:, :].T)
